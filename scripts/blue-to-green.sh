a2d8b79f9d27bb5eeca91f0b2f84281add25a9a3
Ranali Wijendra <86163352+ranaliH@users.noreply.github.com>
2024-12-24 23:47:09 +0530

    Update blue-to-green.sh

--- scripts/blue-to-green.sh
+++ scripts/blue-to-green.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+kubectl apply -f green-deployment.yaml
+kubectl set image deployment/patient-record-service-blue patient-record-service=ranaliw/patient-record-service:${GITHUB_SHA} --record
 
